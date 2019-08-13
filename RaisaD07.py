2e91105b7b056fc6ba02edbac156b5717f0c8977
Raisa2597 <54040158+Raisa2597@users.noreply.github.com>
2019-08-13 13:45:03 -0400

    Add files via upload

---
+++ RaisaD07.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+created on Mon aug 12 11:24:34 2019
+This is a temporary script file.
+"""
+#(0719
+print('hello world')#ld09
+print("d0r7a09r1i9")
+print("raisa")
+5*2
+5/2
+5-2
+5^2
+17+17
+def add(a,b):
+ add = a+b
+ print(add)
+ add(2,6)
+ add(10985.345,9802.472)
+ add(20419.087,069.112)
+ def adds(a,b,c):
+     adds = a+b+c
+     print(adds)
+ adds(112,342,69)
+cadbury1="milk chocolate"
+cadbury2="dark chocolate"
+cadbury3="white chocolate"
+cadbury07="Dogs"
+cadbury09="York"
+cadbury2019="Rock"
+print("Red")
+print("Yellow")
+print("Dark")
+def aqua(07,09,19):
+    aqua = 07*09*19
+    print("The product of ",07,09," "and" ",19," is "aqua")
+def area(1/2,b,h)
+    area = 1/2*b*h
+    print("The product of ",1/2,b," "and" ",h," is "area")
+    
+def area(b,h)
+    area = 1/2*b*h
+    print(area)
+area(1,2)
+    
+    
+aqua(1/2,35,12)
+start
+    
+    
+    
+    
+    
+    
